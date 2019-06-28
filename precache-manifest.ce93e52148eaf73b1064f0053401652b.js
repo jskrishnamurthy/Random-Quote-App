89da6d17e7a1a8675a2dbfea1df3626107cec74c
jayKrishnamurthy <jayasimhan.k@gmail.com>
2019-06-28 15:04:57 -0700

    Updates

---
+++ precache-manifest.ce93e52148eaf73b1064f0053401652b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "232da4f1d16ac2a1e3396cc35d7e93ed",
+    "url": "/React-Random-Quote-App/index.html"
+  },
+  {
+    "revision": "55db0a9944b55dd9079e",
+    "url": "/React-Random-Quote-App/static/css/main.8e5299f1.chunk.css"
+  },
+  {
+    "revision": "b42aa7dd048f4b5d252a",
+    "url": "/React-Random-Quote-App/static/js/2.5c4e487f.chunk.js"
+  },
+  {
+    "revision": "55db0a9944b55dd9079e",
+    "url": "/React-Random-Quote-App/static/js/main.76a05e87.chunk.js"
+  },
+  {
+    "revision": "07804982932362c59a8e",
+    "url": "/React-Random-Quote-App/static/js/runtime~main.17dfb876.js"
+  }
+]);
